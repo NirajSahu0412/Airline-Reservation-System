fef9576078d4ca68008c997bd953b9414a1f133e
lyricMirror <nirajsahu0412@gmail.com>
2023-02-24 22:57:37 +0530

    commit

--- src/PrintTicket1.java
+++ src/PrintTicket1.java
@@ -8,10 +8,7 @@ public class PrintTicket1 extends JFrame
 	{
 		Container c=getContentPane();
 		c.setLayout(new BorderLayout());
-
-
 		JPanel Panel2 = new JPanel(null);
-
 		Panel2.setPreferredSize(new Dimension(500,200));
 
 		JLabel LTitle = new JLabel("<html><b><font color=\"#C71585\",size=\"7\">AirLine Ticket</font></b></html>");
@@ -25,22 +22,14 @@ public class PrintTicket1 extends JFrame
 		JLabel LChildren = new JLabel("<html><font color=\"#000000\">Children &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; : &nbsp;</font><font color=\"#778899\">"+ iChildren+"</font></html>" );
 		JLabel LInfant = new JLabel("<html><font color=\"#000000\">Infant &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;: &nbsp;</font><font color=\"#778899\">"+iInfant+"</font></html>" );
 		JLabel LWishes = new JLabel("<html><body><I><font color=\"#D2B48C\">Wish you a happy journy</font></I></body></html>");
-
-
-		// JLabel LTicketNo=new JLabel("<html><font color=\"#000000\">TicketNumber &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;: &nbsp;</font></html>");
-		// JLabel LBookedBy=new JLabel("<html><font color=\"#000000\">BookedBy &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;: &nbsp;</font></html>");
-
 		JLabel LEmpty = new JLabel("---------------------------------------------------------------------------------------------------------------------------------------------------------------------------");
 		JLabel LDemo = new JLabel("<html><U><font color=\"#8FBC8F\">AirLine-Project Developed By</font></U></html>");
 		JLabel Amit=new JLabel("<html><I><font color=\"#8FBC8F\">Amit Singh</font></I></html>");
 		JLabel Niraj=new JLabel("<html><I><font color=\"#8FBC8F\">Niraj Sahu</font></I></html>");
 		JLabel khushi=new JLabel("<html><I><font color=\"#8FBC8F\">Khushi</font></I></html>");
-		// JLabel LSathya=new JLabel("<html><I><font color=\"#8FBC8F\">Sathyaraj</font></I></html>");
-
 
 		LTitle.setBounds(170,15,500,45);
 		LFrom.setBounds(20,80,300,20);
-
 		LTo.setBounds(20,125,300,20);
 		LClass.setBounds(20,170,300,20);
 		LBookingDate.setBounds(20,215,300,20);
@@ -49,21 +38,12 @@ public class PrintTicket1 extends JFrame
 		LAdult.setBounds(20,345,300,20);
 		LChildren.setBounds(20,385,300,20);
 		LInfant.setBounds(20,430,300,20);
-
 		LWishes.setBounds(530,435,300,20);
-
-
-		// LTicketNo.setBounds(320,80,300,20);
-		// LBookedBy.setBounds(320,125,300,20);
-
 		LEmpty.setBounds(3,445,1000,20);
-
 		LDemo.setBounds(280,465,300,20);
 		Amit.setBounds(285,485,300,20);
 		Niraj.setBounds(285,505,300,20);
 		khushi.setBounds(285,525,300,20);
-		// LSathya.setBounds(285,545,300,20);
-
 
 		Panel2.add(LTitle);
 		Panel2.add(LFrom);
@@ -75,28 +55,15 @@ public class PrintTicket1 extends JFrame
 		Panel2.add(LInfant);
 		Panel2.add(LPrice);
 		Panel2.add(LTime);
-		//Panel2.add(LEmpty);
-
 		Panel2.add(LWishes);
-
-		// Panel2.add(LTicketNo);
-		// Panel2.add(LBookedBy);
 		Panel2.add(LEmpty);
 		Panel2.add(LDemo);
-
-
 		Panel2.add(Amit );
 		Panel2.add(Niraj);
 		Panel2.add(khushi);
-		// Panel2.add(sunil);
-
-
 		Panel2.setBackground(Color.white);
 
 		c.add(Panel2, BorderLayout.CENTER);
-
-
-		//pack();
 		setSize(700,650);
 		setVisible(true);
 	}
